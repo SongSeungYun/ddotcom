f530f4ced8b5c75450b224155f0930d3154a78fd
SongSeungYun <singsong9984@naver.com>
2025-02-11 22:16:42 +0900

    initial state

---
+++ ddotcom/backend/src/main/kotlin/ddotcom/ddotcom/DdotcomApplication.kt
@@ -0,0 +1,11 @@
+package ddotcom.ddotcom
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DdotcomApplication
+
+fun main(args: Array<String>) {
+	runApplication<DdotcomApplication>(*args)
+}
